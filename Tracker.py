1d8ef6257ffceb67f4b8ec72357b03e5ab77afc8
Jari-Daemen <82100834+Jari-Daemen@users.noreply.github.com>
2021-04-16 01:45:27 +0200

    program now saves resultts to .txt file

--- Tracker.py
+++ Tracker.py
@@ -5,6 +5,7 @@ import bs4 as bs
 import re
 from collections import Counter
 from datetime import datetime
+import os
 
 active = True
 comments = []
@@ -12,14 +13,22 @@ sub = []
 tickers = []
 start_flag = True
 
+# creating a text file containing the info
+
+file_date = str(datetime.now())
+file_date = file_date[0:10]
+
+
 # specify a starting time for the program
 
 while start_flag:
     start_time = str(datetime.now())
     start_time = start_time[11:16]
-    if start_time == '23:35':
+
+    if start_time == '10:02':
         print('The program has begun')
         start_flag = False
+
 
 # The full loop
 
@@ -27,16 +36,22 @@ while active:
 
     time = str(datetime.now())
     time = time[11:16]
-    print(time)
+
 
     # Specify a stopping time
 
-    if time == '23:42':
+    if time == '11:02':
         final_list = sorted(occurences.items(), key=lambda x: int(x[0]))
         final_list = sorted(final_list, reverse=True)
         print(final_list)
-        print('The program has finished')
-        break
+        print('The program has finished, storing results...')
+        if os.path.isfile(f'Tickers {file_date}.txt'):
+            print('File already exists!')
+            break
+        else:
+            with open(f'Tickers {file_date}.txt', 'w') as docu:
+                docu.write(final_list)
+                break
 
     # source and class may need to be adjusted (automatically)
 
