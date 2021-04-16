20a71b492bdecffed0c537ed41d21dc5c1c49b2a
Jari-Daemen <82100834+Jari-Daemen@users.noreply.github.com>
2021-04-16 12:59:34 +0200

    Fixed .txt issue

--- Tracker.py
+++ Tracker.py
@@ -5,12 +5,14 @@ import bs4 as bs
 import re
 from collections import Counter
 from datetime import datetime
+# from time import sleep
 import os
 
 active = True
 comments = []
 sub = []
 tickers = []
+skip = []
 start_flag = True
 
 # creating a text file containing the info
@@ -18,17 +20,15 @@ start_flag = True
 file_date = str(datetime.now())
 file_date = file_date[0:10]
 
-
 # specify a starting time for the program
 
 while start_flag:
     start_time = str(datetime.now())
     start_time = start_time[11:16]
 
-    if start_time == '10:32':
+    if start_time == '12:08':
         print('The program has begun')
         start_flag = False
-
 
 # The full loop
 
@@ -37,10 +37,9 @@ while active:
     time = str(datetime.now())
     time = time[11:16]
 
-
     # Specify a stopping time
 
-    if time == '10:58':
+    if time == '12:28':
         final_list = sorted(occurences.items(), key=lambda x: int(x[0]))
         final_list = sorted(final_list, reverse=True)
         print(final_list)
@@ -58,14 +57,28 @@ while active:
     # link has to be replaced every page, classes are constant
 
     source = r.get('''
-    https://www.reddit.com/r/wallstreetbets/comments/mrms60/what_are_your_moves_tomorrow_april_16_2021/'''
+    https://www.reddit.com/r/wallstreetbets/comments/mrzzt0/daily_discussion_thread_for_april_16_2021/'''
                    ).text
 
     soup = bs.BeautifulSoup(source, features='html.parser')
     match = soup.find('div', class_="_3tw__eCCe7j-epNCKGXUKk")
 
+    #stickied = soup.find('span', class_="_2ETuFsVzMBxiHia6HfJCTQ _2wd-K5Djdc9TGPRGDgmkpX")
+    #if stickied:
+    #    tmp = str(match)
+    #    tmp = tmp[1314:-4351]
+    #    if tmp in sub:
+    #        pass
+    #    else:
+    #        sub.append(tmp)
+    #        print(sub)
+
     if match == None:
         continue
+
+    #elif tmp in sub:
+    #    print('loser')
+    #    continue
 
     else:
         comment = str(match.find('p', class_="_1qeIAgB0cPwnLhDF9XSiJM"))
