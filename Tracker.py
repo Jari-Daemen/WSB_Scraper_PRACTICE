72682bb5fc2d5d7586844c3256c7109b40ef9046
Jari-Daemen <82100834+Jari-Daemen@users.noreply.github.com>
2021-04-16 14:38:58 +0200

    Final version Tracker.py

--- Tracker.py
+++ Tracker.py
@@ -1,24 +1,17 @@
-# Counts the amount of mentions of a certain ticker
+# Counts the amount of mentions of a certain ticker on WSB daily discussion thread
 
 import requests as r
 import bs4 as bs
 import re
 from collections import Counter
 from datetime import datetime
-# from time import sleep
-import os
+
 
 active = True
 comments = []
 sub = []
 tickers = []
-skip = []
 start_flag = True
-
-# creating a text file containing the info
-
-file_date = str(datetime.now())
-file_date = file_date[0:10]
 
 # specify a starting time for the program
 
@@ -26,8 +19,8 @@ while start_flag:
     start_time = str(datetime.now())
     start_time = start_time[11:16]
 
-    if start_time == '12:08':
-        print('The program has begun')
+    if start_time == '14:32':
+        print('The program has started')
         start_flag = False
 
 # The full loop
@@ -39,22 +32,14 @@ while active:
 
     # Specify a stopping time
 
-    if time == '12:28':
-        final_list = sorted(occurences.items(), key=lambda x: int(x[0]))
-        final_list = sorted(final_list, reverse=True)
+    if time == '14:36':
+
+        final_list = sorted(occurences.items(), key=lambda x: (x[0])) #sorts amount of mentions from low to high
         print(final_list)
-        print('The program has finished, storing results...')
-        if os.path.isfile(f'Tickers {file_date}.txt'):
-            print('File already exists!')
-            break
-        else:
-            with open(f'Tickers {file_date}.txt', 'w') as docu:
-                docu.write(final_list)
-                break
+        print('The program has finished')
+        break
 
-    # source and class may need to be adjusted (automatically)
-
-    # link has to be replaced every page, classes are constant
+    # link has to be replaced, classes are constant
 
     source = r.get('''
     https://www.reddit.com/r/wallstreetbets/comments/mrzzt0/daily_discussion_thread_for_april_16_2021/'''
@@ -63,22 +48,8 @@ while active:
     soup = bs.BeautifulSoup(source, features='html.parser')
     match = soup.find('div', class_="_3tw__eCCe7j-epNCKGXUKk")
 
-    #stickied = soup.find('span', class_="_2ETuFsVzMBxiHia6HfJCTQ _2wd-K5Djdc9TGPRGDgmkpX")
-    #if stickied:
-    #    tmp = str(match)
-    #    tmp = tmp[1314:-4351]
-    #    if tmp in sub:
-    #        pass
-    #    else:
-    #        sub.append(tmp)
-    #        print(sub)
-
     if match == None:
         continue
-
-    #elif tmp in sub:
-    #    print('loser')
-    #    continue
 
     else:
         comment = str(match.find('p', class_="_1qeIAgB0cPwnLhDF9XSiJM"))
@@ -92,20 +63,13 @@ while active:
             print(comment)
 
             for element in comments:
-                a = re.compile(r'[A-Z]{3,5}')
+                a = re.compile(r'[A-Z]{2,5}')
                 tmp = a.findall(element)
                 comments.remove(comment)
                 sub.append(comment)
 
                 for ticker in tmp:
                     tickers.append(ticker)
-                    print(tickers)
                     occurences = Counter(tickers)
-                    print(occurences)
-
-# prints the final list of tuples in descending key value
 
 
-# final_list = sorted(occurences.items(), key=lambda x: int(x[0]))
-# final_list = sorted(final_list, reverse=True)
-# print(final_list)
